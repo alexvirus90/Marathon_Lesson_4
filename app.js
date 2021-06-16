4d3f2f2fb5a5d71d0ffaed25267ee66ea831a5aa
Alexandr Komarov <alexandr.komarov@veeam.com>
2021-06-16 16:27:05 +0700

    переписал функции

--- app.js
+++ app.js
@@ -15,25 +15,27 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
 
-  square.addEventListener('mouseover', () => setColor(square))
-
-  square.addEventListener('mouseleave', () => removeColor(square))
+  square.addEventListener('mouseover', setColor)
+  square.addEventListener('mouseleave', removeColor)
 
   board.append(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+  const { target } = event
   const color = getRandomColor()
-  element.style.backgroundColor = color
-  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+  target.style.backgroundColor = color
+  target.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
-  element.style.backgroundColor = '#1d1d1d'
-  element.style.boxShadow = `0 0 2px #000`
+function removeColor(event) {
+  const { target } = event
+
+  target.style.backgroundColor = '#1d1d1d'
+  target.style.boxShadow = `0 0 2px #000`
 }
 
 function getRandomColor() {
-  const indexColor = Math.floor(Math.random() * colors.length)
-  return colors[indexColor]
+  return colors[Math.floor(Math.random() * colors.length)]
 }
